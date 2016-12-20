f72473e0b688153bcdc1397822f5b13f56ef528c
Ser-Castic <samsteinberg90@gmail.com>
2016-12-20 14:56:49 -0500

    Added Port

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -2,9 +2,15 @@ package com.theironyard.charlotte;
 
 import spark.Spark;
 
+
 public class Main {
 
     public static void main(String[] args) {
+        String port = System.getenv("PORT");
+
+        if (port != null) {
+            Spark.port(Integer.valueOf(port));
+        }
 
         Spark.before((request, response) -> {
             response.header("Access-Control-Allow-Origin", "*");
